f6e74fe2a66d8782fa1f2797074998e7e0e7ae4d
Aweirdanomaly <carlosarielos2@gmail.com>
2021-09-23 13:28:38 -0400

    Made PowerPlant

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,3 +1,25 @@
+import pip
+import subprocess
+import sys
+import os
+
+
 def draw_power_plant():
-    print("power plant not found")
+    img = """          ) ) )                     ) ) )
+        ( ( (                      ( ( (
+      ) ) )                       ) ) )
+   (~~~~~~~~~)                 (~~~~~~~~~)
+    | POWER |                   | POWER |
+    |       |                   |       |
+    I      _._                  I       _._
+    I    /'   `\                I     /'   `\\
+    I   |   N   |               I    |   N   |
+    f   |   |~~~~~~~~~~~~~~|    f    |    |~~~~~~~~~~~~~~|
+  .'    |   ||~~~~~~~~|    |  .'     |    | |~~~~~~~~|   |
+/'______|___||__###___|____|/'_______|____|_|__###___|___|"""
+
+    print("\n\nThe massive power plant stands before you. You can \
+hear a faint humming as you approach it and you see massive amounts of steam exiting\
+the biggest towers")
+    print(img)
     return
